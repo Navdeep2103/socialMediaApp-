3fd176a841797cced75a55d92aaf3d8c69ccd358
Navdeep Kaur <86navdeep.k@gmail.com>
2018-11-14 18:31:19 -0800

    deployment

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports={
-    MongoURI:'mongodb://production:Roses7071@ds255403.mlab.com:55403/socialmedia-prod',
-    GoogleClientID:'853563347634-r1o2pbs7hv88l8pa4kvud1014vl0daao.apps.googleusercontent.com',
+    MongoURI:process.env.MONGO_URI,
+    GoogleClientID:process.env.GOOGLE_CLIENT_ID,
     GoogleClientSecret:'7KpGYi7Xk57e3WG1xrgq-tTs'
 }

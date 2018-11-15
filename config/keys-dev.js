7622c0db6374cd5d952ee282280dc00aeb919c65
Navdeep Kaur <86navdeep.k@gmail.com>
2018-11-15 14:42:07 -0800

    deployment

---
+++ config/keys-dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoURI:'mongodb://socialmedia:Roses7071@ds155653.mlab.com:55653/socialmedia',
+    GoogleClientID:'112375071394-i9a3hg53meebnpt4ttlcrm8ej9puo4l3.apps.googleusercontent.com',
+    GoogleClientSecret:'DJYZ6Fi4vE7yeKB5Qf5swtKo',
+    FacebookClientID:'715521092151203',
+    FacebookClientSecret:'d2317429d6209219266e16eccaad177f'
+}

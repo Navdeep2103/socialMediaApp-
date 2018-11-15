0a4361414519535c7726db072e87bf282b3b785f
Navdeep Kaur <86navdeep.k@gmail.com>
2018-11-14 20:57:53 -0800

    deployment

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports={
     MongoURI:process.env.MONGO_URI,
     GoogleClientID:process.env.GOOGLE_CLIENT_ID,
-    GoogleClientSecret:'7KpGYi7Xk57e3WG1xrgq-tTs'
+    GoogleClientSecret:process.env.GOOGLE_CLIENT_SECRET
 }
